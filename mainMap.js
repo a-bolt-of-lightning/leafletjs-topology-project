06be84504b1367faea41902c4bc863b1509f58b7
F Mahvari <teamoonshine@gmail.com>
2020-04-26 23:30:51 +0430

    added style for demo - main files are ready fro demo

--- mainMap.js
+++ mainMap.js
@@ -1,5 +1,4 @@
 
-
 /*
 the following codes are to be injected into the html file:
  
@@ -32,22 +31,22 @@ inside body:
 
 //dummy data
 var dummyData = [
-    [
-        [33.51, 55.68],
-        [33.51, 57.68]
-    ],
-    [
-        [32.44, 50.40],
-        [30.30, 60.00]
-    ],
-    [
-        [30.51, 55.68],
-        [33.51, 57.68]
-    ],
-    [
-        [32.44, 56.40],
-        [34.30, 60.00]
-    ]
+    // [
+    //     [33.51, 55.68],
+    //     [33.51, 57.68]
+    // ],
+    // [
+    //     [32.44, 50.40],
+    //     [30.30, 60.00]
+    // ],
+    // [
+    //     [30.51, 55.68],
+    //     [33.51, 57.68]
+    // ],
+    // [
+    //     [32.44, 56.40],
+    //     [34.30, 60.00]
+    // ]
 ];
 
 
@@ -95,9 +94,8 @@ wrapper.appendChild(displayArea);
 
 
 //dummy markers
-var marker = L.marker([33.51, 55.68]).addTo(mymap);
-var marker = L.marker([34.51, 54.68]).addTo(mymap);
-
+// var marker = L.marker([33.51, 55.68]).addTo(mymap);
+// var marker = L.marker([34.51, 54.68]).addTo(mymap);
 
 
 drawLines(dummyData, handleMouseOverLines);
@@ -107,14 +105,16 @@ createLegend(mymap);
 // handleMouseOverLines();
 
 
-const drawBtn = document.getElementById("drawLinkBtn");
+// const drawBtn = document.getElementById("drawLinkBtn");
 // drawBtn.addEventListener("click", e => drawLinkBetweenNodes(e, mymap));
 // drawLinkBetweenNodes(mymap);
 
 
-const addBtn = document.getElementById("addNodeToMainMap");
+// const addBtn = document.getElementById("addNodeToMainMap");
 // addBtn.addEventListener("click", addNodeToMainMap);
-console.log(addNodeToMainMap(mymap));
+var dataVar;
+console.log(addNodeToMainMap(mymap, dataVar));
+console.log(dataVar);
 
 
 /**
@@ -143,7 +143,6 @@ function createLegend(mymap) {
 
     legend.addTo(mymap);
 }
-
 
 
 
@@ -245,79 +244,439 @@ function unshowLineNumberInBox() {
 
 //////////
 
-function drawLinkBetweenNodes(mymap) {
+// function drawLinkBetweenNodes(mymap) {
 
 
-    // var res = mymap.on("click", event => addLink(event));
+//     // var res = mymap.on("click", event => addLink(event));
 
-    // console.log('wtf');
-    // console.log(res);
+//     // console.log('wtf');
+//     // console.log(res);
 
-    return 4;
+//     return 4;
 
+// }
 
-}
+function addNodeToMainMap(mymap, dataVar) {
 
-function addLink(e) {
-    console.log(e);
-}
+    var markers = [];
+    var links = [];
 
+    var markerList = [];
 
-function addNodeToMainMap(mymap) {
+    featureGroup = L.featureGroup();
+    console.log(featureGroup);
+    featureGroup.addTo(mymap);
 
-    var sendBack = false;
-
-    var data;
-    var marker;
+    var addNodeForm = createAddNodeForm(featureGroup, markers, markerList, mymap);
+    var addLinkForm = createAddLinkForm(featureGroup, links, markerList, mymap);
 
     var div = document.createElement("div");
+    div.setAttribute("id", "topology-panel");
 
     var menu = L.control({ position: 'topright' });
-    var remBtn = document.createElement("button");
-    var createBtn = document.createElement("button");
+    var closeBtn = document.createElement("button");
+    closeBtn.setAttribute("class", "mainmap-util-closebtn");
+    var addNodeBtn = document.createElement("button");
+    addNodeBtn.setAttribute("class", "mainmap-util-btn");
+    var addLinkbtn = document.createElement("button");
+    addLinkbtn.setAttribute("class", "mainmap-util-btn");
     var doneBtn = document.createElement("button");
-    // doneBtn.addEventListener("click", e => console.log("done"));
-    remBtn.addEventListener("click", e =>div.remove());
-    createBtn.addEventListener("click", e => {
-        marker = L.marker(mymap.getCenter(), {draggable: true}).addTo(mymap);
-    });
-    doneBtn.addEventListener("click", e => {
-        sendBack = true;
-        //set marker coords and data to some variable
-        marker.dragging.disable();
-        div.remove();
-        console.log(data);  
-        return data;
+    doneBtn.setAttribute("class", "mainmap-util-btn");
 
+    closeBtn.addEventListener("click", e => {
+
+        markers = [];
+        markerList = [];
+        links = [];
+        featureGroup.remove();
+
+        div.remove();
     });
-    var dataIn = document.createElement("input");
-    dataIn.setAttribute("type", "text");
-    dataIn.addEventListener("input", e => data = e.target.value);
-    doneBtn.innerHTML = "Done!";
-    createBtn.innerHTML = "Create!";
-    remBtn.innerHTML = "x";
+
+
+    addNodeBtn.addEventListener("click", e => {
+
+        if (document.getElementById("addLinkForm").style.display === "block")
+            return;
+        document.getElementById("addNodeForm").style.display = "block";
+    });
+
+
+
+    doneBtn.addEventListener("click", e => {
+        // sendBack = true;
+        //set marker coords and data to some variable
+        featureGroup.eachLayer(layer => {
+            if (layer instanceof L.Marker)
+                layer.dragging.disable();
+        });
+
+        div.remove();
+        console.log(markers);
+        console.log(links);
+        dataVar = {
+            "nodes": markers,
+            "links": links,
+            "featureGroup": featureGroup
+        }
+    });
+
+    doneBtn.innerHTML = "Done";
+    addNodeBtn.innerHTML = "Add Node";
+    closeBtn.innerHTML = "x";
+    addLinkbtn.innerHTML = "Add Link"
+
+    addLinkbtn.addEventListener("click", e => {
+
+        if (document.getElementById("addNodeForm").style.display === "block")
+            return;
+        document.getElementById("addLinkForm").style.display = "block";
+    });
 
     menu.onAdd = function (map) {
 
-        div.appendChild(createBtn);
         div.appendChild(doneBtn);
-        div.appendChild(remBtn);
-        div.appendChild(dataIn);
+        div.appendChild(addLinkbtn);
+        div.appendChild(addNodeBtn);
+        div.appendChild(closeBtn);
+        div.appendChild(addNodeForm);
+        div.appendChild(addLinkForm);
 
         return div;
     };
 
     menu.addTo(mymap);
-
-    // if(sendBack){
-    //     return data;
-    // }
-    //looks like i'm not gonna use this :(
-    // mymap.once("click", e => {
-    //     var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
-    // });
 }
 
-function createDraggableMarker(data, mymap) {
 
+function createAddNodeForm(featureGroup, markers, markerList, mymap) {
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "addNodeForm");
+    div.setAttribute("class", "vertical");
+
+    var nodeParamLbl = document.createElement("label");
+    nodeParamLbl.setAttribute("class", "mainmap-util-label");
+    nodeParamLbl.innerHTML = "ROADM_Type: ";
+
+    var nodeParam = document.createElement("input");
+    nodeParam.setAttribute("id", "ROADM_Type");
+    nodeParam.setAttribute("class", "mainmap-util-input");
+    nodeParam.value = "Directionless";
+    nodeParam.placeholder = "Directionless";
+    nodeParam.setAttribute("type", "text");
+
+    var doneBtn = document.createElement("button");
+    doneBtn.setAttribute("id", "submitNodeForm");
+    doneBtn.setAttribute("class", "mainmap-util-btn");
+    doneBtn.innerHTML = "Done";
+
+    var closeBtn = document.createElement("button");
+    closeBtn.setAttribute("id", "closeNodeForm");
+    closeBtn.setAttribute("class", "mainmap-util-btn");
+    closeBtn.innerHTML = "Close";
+
+    closeBtn.addEventListener("click", e => {
+        console.log(featureGroup);
+        document.getElementById("addNodeForm").style.display = "none";
+    });
+
+    doneBtn.addEventListener("click", e => {
+
+        var nodeData;
+        var marker;
+
+        var srcNodepopup = L.popup({ autoClose: false }).setContent("Source Node");
+        var destNodepopup = L.popup({ autoClose: false }).setContent("Destination Node");
+        // destNodepopup.setLatLng([33.51, 57.68]).openOn(mymap);
+
+        console.log("llll");
+        nodeData = onSubmitNodeForm();
+        marker = L.marker(mymap.getCenter(), { draggable: true });
+
+        marker.on("click", e => {
+            console.log(e.target);
+            markerList.push(e.target);
+            // var isSrc = setLinkSrcAndDest(markerList, marker);
+            // if (isSrc) srcNodepopup.setLatLng(e.target.getLatLng()).openOn(mymap);
+            // else destNodepopup.setLatLng(e.target.getLatLng()).openOn(mymap);
+        });
+
+
+        var connectedLinks = [];
+        var markerInitLatLng;
+
+        marker.on("dragstart", e => {
+
+            connectedLinks = []
+            // console.log(e.target.getLatLng());
+            markerInitLatLng = e.target.getLatLng();
+
+            getConnectedLinks(e.target, featureGroup, connectedLinks);
+        });
+
+        marker.on("drag", e => {
+
+            console.log(e.target.getLatLng());
+            markerInitLatLng = connectLinkToNode(e.target, connectedLinks, markerInitLatLng);
+        })
+
+        marker.on("dragend", e => {
+
+            markerInitLatLng = connectLinkToNode(e.target, connectedLinks, markerInitLatLng);
+        });
+
+        featureGroup.addLayer(marker);
+        markers.push({
+            "node": marker,
+            "nodeData": nodeData
+        });
+        // console.log(markers);
+        div.style.display = "none";
+    });
+
+    div.appendChild(nodeParamLbl);
+    div.appendChild(nodeParam);
+    div.appendChild(doneBtn);
+    div.appendChild(closeBtn);
+    div.style.display = "none";
+    return div;
+}
+
+function onSubmitNodeForm() {
+
+    return {
+        "ROADM_Type": document.getElementById("ROADM_Type").value,
+    };
+}
+
+function createAddLinkForm(featureGroup, links, markerList, mymap) {
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "addLinkForm");
+    div.setAttribute("class", "vertical");
+
+    var paramValues = ["0", "0.2", "0", "1.40E-03", "3.16914E-19"];
+    var paramNames = ["Fiber_Type", "Loss_Coefficient", "Beta", "Gamma", "Dispersion"];
+
+
+    //create link Params
+    linkParams = createLinkParams(paramValues, paramNames);
+    console.log(linkParams);
+
+    var doneBtn = document.createElement("button");
+    doneBtn.setAttribute("id", "submitLinkForm");
+    doneBtn.setAttribute("class", "mainmap-util-btn");
+    doneBtn.innerHTML = "Done";
+
+    var closeBtn = document.createElement("button");
+    closeBtn.setAttribute("id", "closeLinkForm");
+    closeBtn.setAttribute("class", "mainmap-util-btn");
+    closeBtn.innerHTML = "Close";
+
+    closeBtn.addEventListener("click", () => {
+        document.getElementById("addLinkForm").style.display = "none";
+    });
+
+    doneBtn.addEventListener("click", () => {
+
+        var linkData;
+
+        if (markerList.length < 2) {
+            popupAlert("choose 2 nodes", mymap);
+            return;
+        }
+
+        if (markerList[markerList.length - 1] === markerList[markerList.length - 2]) {
+            popupAlert("choose a destination node", mymap);
+            return;
+        }
+
+        latlngs = [
+            markerList[markerList.length - 1].getLatLng(),
+            markerList[markerList.length - 2].getLatLng(),
+        ];
+        // console.log(latlngs);
+
+        var link = L.polyline(latlngs, { color: 'red' });
+        featureGroup.addLayer(link);
+
+        linkData = onSubmitLinkForm(paramValues, paramNames);
+
+        links.push({
+            "link": link,
+            "linkData": linkData
+        });
+
+        // console.log(links);
+        div.style.display = "none";
+
+    });
+
+    //add link params list
+    for(var i=0; i<linkParams.length; i++){
+        div.appendChild(linkParams[i].label);
+        div.appendChild(linkParams[i].param);
+    }
+
+    div.appendChild(doneBtn);
+    div.appendChild(closeBtn);
+
+    div.style.display = "none";
+    return div;
+}
+
+function onSubmitLinkForm(paramValues, paramNames) {
+
+    linkParams = [];
+
+    for(var i=0; i<paramValues.length; i++){
+        var param = document.getElementById(paramNames[i]).value;
+        //checking for validity
+        linkParams.push(param);
+    }
+    return linkParams;
+}
+
+
+function popupAlert(msg, mymap) {
+
+    var div = document.createElement("div");
+    div.setAttribute("class", "alert horizontal");
+    var alertTxt = document.createElement("h5");
+    var closeBtn = document.createElement("button");
+
+    alertTxt.innerHTML = msg;
+    closeBtn.innerHTML = "X";
+    closeBtn.setAttribute("class", "mainmap-util-btn");
+
+    closeBtn.addEventListener("click", () => div.remove());
+
+    var alertBox = L.control({ position: 'bottomright' });
+
+    alertBox.onAdd = function (map) {
+        div.appendChild(alertTxt);
+        div.appendChild(closeBtn);
+        return div;
+    };
+
+    alertBox.addTo(mymap);
+    console.log(msg);
+}
+
+
+function connectLinkToNode(marker, connectedLinks, markerInitLatLng) {
+
+    for (var i = 0; i < connectedLinks.length; i++) {
+        var link = connectedLinks[i];
+        console.log("ll" + link.getLatLngs());
+        console.log("ma" + marker.getLatLng());
+
+        var latLngStart = link._latlngs[0];
+        var latLngEnd = link._latlngs[1];
+
+        if (latLngEnd == markerInitLatLng) {
+
+            console.log("ee");
+            link.setLatLngs([
+                link.getLatLngs()[0],
+                marker.getLatLng()
+            ]);
+        }
+        else if (latLngStart == markerInitLatLng) {
+
+            console.log("ss");
+            link.setLatLngs([
+                marker.getLatLng(),
+                link.getLatLngs()[1]
+            ]);
+        }
+    }
+    console.log("init " + markerInitLatLng);
+    return marker.getLatLng();
+
+
+}
+
+function getConnectedLinks(marker, featureGroup, connectedLinks) {
+
+    featureGroup.eachLayer(layer => {
+        if (layer instanceof L.Polyline) {
+
+            console.log(layer._latlngs);
+            if (layer._latlngs.includes(marker.getLatLng())) {
+                console.log("layer" + layer._latlngs);
+                connectedLinks.push(layer)
+            }
+        }
+
+    });
+}
+
+function setLinkSrcAndDest(markerList, marker) {
+
+    var isSrc = true;
+
+    if (markerList[0] == null) {
+        if (markerList[0] === marker)
+            return;
+        markerList.splice(0, 0, marker);
+        console.log("src n");
+    }
+    else if (markerList[1] == null) {
+        if (markerList[1] === marker)
+            return;
+        markerList.splice(1, 0, marker);
+        isSrc = false;
+        console.log("des n");
+    }
+    else {
+        markerList.splice(0, 0, marker);
+        markerList.splice(1, 0, null);
+        console.log("b fu");
+    }
+
+    return isSrc;
+}
+
+function createLinkParams(paramValues, paramNames) {
+
+    var paramLblTxts = ["Fiber Type: ", "Loss Coefficient: ", "Beta: ", "Gamma: ", "Dispersion: "];
+
+    var paramElements = [];
+
+    for(var i=0; i<paramNames.length; i++){
+        var param = document.createElement("input");
+        param.setAttribute("id", paramNames[i]);
+        param.setAttribute("class", "mainmap-util-input");
+        param.value = paramValues[i];
+        param.placeholder = paramValues[i];
+        param.setAttribute("type", "text");
+
+        var paramLbl = document.createElement("label");
+        paramLbl.setAttribute("class", "mainmap-util-label");
+        paramLbl.innerHTML = paramLblTxts[i];
+
+        paramElements.push({
+            "param": param,
+            "label": paramLbl
+        });
+    }
+
+    return paramElements;
+
+
+    // var linkParam1 = document.createElement("input");
+    // linkParam1.setAttribute("id", "Fiber_Type");
+    // linkParam1.setAttribute("class", "mainmap-util-input");
+    // linkParam1.value = "param1-link";
+    // linkParam1.placeholder = "param1-link";
+    // linkParam1.setAttribute("type", "text");
+
+    // var linkParam2 = document.createElement("input");
+    // linkParam2.setAttribute("id", "linkParam2");
+    // linkParam2.setAttribute("class", "mainmap-util-input");
+    // linkParam2.value = "param2-link";
+    // linkParam2.placeholder = "param2-link";
+    // linkParam2.setAttribute("type", "text");
 }
